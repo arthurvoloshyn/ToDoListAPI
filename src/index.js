70b0656ff3fe8cd91b39dbb32482ab168a8604d9
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-06-11 15:24:30 +0300

    Delete unused files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { StrictMode } from 'react';
+import { render } from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
+render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
