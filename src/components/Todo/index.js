77e46746c0f00cbcb47f9fc3d4c5157fc17a10e3
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-06-13 17:38:21 +0300

    Update Route Matches

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
 import TodoButton from '../TodoButton';
 import TodoList from '../TodoList';
 
-const Todo = () => (
-  <>
-    <TodoButton href="/tasks/new">Create a new todo</TodoButton>
-    <TodoList />
-  </>
-);
+const Todo = () => {
+  const { url } = useRouteMatch();
+
+  return (
+    <>
+      <TodoButton href={`${url}/new`}>Create a new todo</TodoButton>
+      <TodoList />
+    </>
+  );
+};
 
 export default Todo;
--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useRouteMatch } from 'react-router-dom';
 import { Paper, List, Divider } from '@material-ui/core';
 import { TodosContext } from '../../contexts/todos';
 import TodoItem from '../TodoItem';
 
 const TodoList = () => {
+  const { url } = useRouteMatch();
   const todos = useContext(TodosContext);
   const todosLength = todos.length - 1;
 
@@ -14,7 +15,7 @@ const TodoList = () => {
     <Paper>
       <List>
         {todos.map((todo, i) => (
-          <Link key={todo.id} to={`/tasks/${todo.id}`}>
+          <Link key={todo.id} to={`${url}/${todo.id}`}>
             <TodoItem {...todo} />
             {i < todosLength && <Divider />}
           </Link>
