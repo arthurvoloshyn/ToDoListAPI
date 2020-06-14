3bda5838d62d603f40fa6fdd14fa8e0ea187b90a
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-06-14 18:07:26 +0300

    Update Loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import propTypes from 'prop-types';
 import { Grid, CircularProgress } from '@material-ui/core';
 
-const Loader = ({ size, ...attrs }) => (
-  <Grid alignContent="center" alignItems="center">
-    <CircularProgress {...attrs} size={size} />
+const Loader = ({ size, color, ...attrs }) => (
+  <Grid container justify="center">
+    <CircularProgress {...attrs} size={size} color={color} />
   </Grid>
 );
 
 Loader.propTypes = {
   size: propTypes.string,
+  color: propTypes.string,
 };
 
 Loader.defaultProps = {
   size: 100,
+  color: 'primary',
 };
 
 export default Loader;
